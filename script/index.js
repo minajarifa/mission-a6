aa7a572ce09e7904aa624ecc330e158498ec377f
Your Name <minajmiha02@gmail.com>
2026-02-17 12:50:59 +0600

    index.js added and all data fetch

---
+++ script/index.js
@@ -0,0 +1,15 @@
+// all data fetch here
+const loadLession=()=>{
+    fetch(`https://fakestoreapi.com/products`)
+    .then(res=>res.json())
+    .then(data=>dispalyLession(data))
+}
+const dispalyLession=(lessions)=>{
+console.log(lessions
+    // get the container and empty 
+    // get into the every lession
+    // create element
+    // appent into comtainer 
+)
+}
+loadLession()
